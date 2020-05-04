1544717b2a5708007211a6dfa3355308e58fc1aa
kevin murphy <kevinnealmurphy@gmail.com>
2020-05-04 18:41:42 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,12 @@
 def oxford_comma(array)
  ary = []
  if array.length == 1
- ary = array.join(", " )
+  ary = array[0]
  elsif array.length == 2
- ary = array.join(", " )
+  ary = array.join(" and " )
  else
-   ary = array.join(", " )
+   ary = array[0...-1].join(", " ) << ("and #{array[-1]}")
  end
+ puts ary
+ return ary
 end
