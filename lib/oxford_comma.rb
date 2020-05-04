bc03686a6e9948847609ef04945515f63c35dd40
kevin murphy <kevinnealmurphy@gmail.com>
2020-05-04 18:38:24 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,10 @@
 def oxford_comma(array)
  ary = []
+ if array.length == 1
  ary = array.join(", " )
+ elsif array.length == 2
+ ary = array.join(", " )
+ else
+   ary = array.join(", " )
+ end
 end
