09b614c61fa25fe6972e82bef677e285c516de3d
kevin murphy <kevinnealmurphy@gmail.com>
2020-05-04 18:49:37 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,8 +6,8 @@ def oxford_comma(array)
   ary = array.join(" and " )
  else
    #ary = array[0...-1].join(", " ) << ("and #{array[-1]}")
-   ary = array[array.size - 1].join(", " )
-   #<< ("and #{array[-1]}")
+    ary = array.pop()
+    ary = ary << ("and #{array[-1]}")
  end
  puts ary
  return ary
