bee1141b776a1aa389179483ec1f1829c102e555
kevin murphy <kevinnealmurphy@gmail.com>
2020-05-04 19:04:47 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,14 +1,11 @@
 def oxford_comma(array)
- ary = []
  if array.length == 1
-  ary = array.first
+  array.first
  elsif array.length == 2
-  ary = array.join(" and " )
+  array.join(" and " )
  else
   array[-1].insert(0, "and ")
   array.join(", ")
   ary = array
  end
- puts ary
- return ary
 end
