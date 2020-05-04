cdbc6fb3f2d64c631bc3ec52b9a45339b323b818
kevin murphy <kevinnealmurphy@gmail.com>
2020-05-04 19:00:13 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -8,8 +8,8 @@ def oxford_comma(array)
   ary_b = []
   ary_e = []
   #ary = array[0...-1].join(", " ) << ("and #{array[-1]}")
-  ary_b = array.drop(-1)
-  ary_e = array.drop(array.length - 1)
+  ary_b = array.pop()
+  ary_e = array.unshift(array.length - 1)
   ary << ary_b.join(", ") 
   ary << ("and #{array[-1]}")
  end
