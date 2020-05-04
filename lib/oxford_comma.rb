3a87980b8feb81c27757dcf4f02cdddce74a1280
kevin murphy <kevinnealmurphy@gmail.com>
2020-05-04 19:03:33 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,16 +1,12 @@
 def oxford_comma(array)
  ary = []
  if array.length == 1
-  ary = array[0]
+  ary = array.first
  elsif array.length == 2
   ary = array.join(" and " )
  else
-  ary_b = []
-  ary_e = []
-  ary_b = array.pop()
-  ary_e = array.unshift(array.length - 1)
-  ary << ary_b.join(", ") 
-  ary << ("and #{array[-1]}")
+  ary = array[-1].insert(0, "and ")
+  ary.join(", ")
  end
  puts ary
  return ary
